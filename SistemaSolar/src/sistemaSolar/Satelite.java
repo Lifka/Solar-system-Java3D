6833381d3b3a1b71bca825dd60cbab924a0c124c
jcgallardo <jcgallardomorales@gmail.com>
2016-04-06 13:07:27 +0200

    Modificacion Satelite

--- SistemaSolar/src/sistemaSolar/Satelite.java
+++ SistemaSolar/src/sistemaSolar/Satelite.java
@@ -6,13 +6,18 @@
  */
 package sistemaSolar;
 
+import java.awt.Color;
 import javax.media.j3d.Material;
 
 
 public class Satelite extends Astro {
     
     public Satelite(String nombre, double radio, double distancia, 
-            String archivo_textura, Material material) {
+            String archivo_textura, Material material, Color c,
+            double t_rotacion, double t_traslacion) {
+        super(nombre, radio, distancia, archivo_textura, material, c, t_rotacion, t_traslacion);
+    }
+    public Satelite(String nombre, double radio, double distancia){
         super(nombre, radio, distancia);
     }
     
