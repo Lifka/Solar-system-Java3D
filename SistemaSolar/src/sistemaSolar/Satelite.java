89120ab8a9cfb25057b2a70c77c8b916f061e8bd
jcgallardo <jcgallardomorales@gmail.com>
2016-04-06 13:15:02 +0200

    Modificacion Satelite

--- SistemaSolar/src/sistemaSolar/Satelite.java
+++ SistemaSolar/src/sistemaSolar/Satelite.java
@@ -11,19 +11,26 @@ import javax.media.j3d.Material;
 
 
 public class Satelite extends Astro {
+    private Planeta planeta;
     
     public Satelite(String nombre, double radio, double distancia, 
             String archivo_textura, Material material, Color c,
             double t_rotacion, double t_traslacion) {
         super(nombre, radio, distancia, archivo_textura, material, c, t_rotacion, t_traslacion);
+        this.planeta = null;
     }
     public Satelite(String nombre, double radio, double distancia){
         super(nombre, radio, distancia);
+        this.planeta = null;
     }
     
     public double getDistancia(){
         // DISTANCIA RESPECTO A SU PLANETA
         return distancia;
+    }
+    
+    public void setPlaneta(Planeta p){
+        this.planeta = p;
     }
     
 }
