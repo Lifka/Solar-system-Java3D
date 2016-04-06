16d5d99a32b8fb4e9f12060c693540e63037e2b3
jcgallardo <jcgallardomorales@gmail.com>
2016-04-06 12:32:14 +0200

    Implementación planeta

--- SistemaSolar/src/sistemaSolar/Planeta.java
+++ SistemaSolar/src/sistemaSolar/Planeta.java
@@ -5,6 +5,9 @@
  */
 package sistemaSolar;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
 import javax.media.j3d.Material;
 
 /**
@@ -12,11 +15,58 @@ import javax.media.j3d.Material;
  * @author jgallardo
  */
 public class Planeta extends Astro{
+    Estrella estrella;
+    HashMap<String,Satelite> satelites;
     
-    public Planeta(String nombre, double radio, double distancia, String archivo_textura, Material material) {
-        super(nombre, radio, distancia);
+    public Planeta(String nombre, double radio, double distancia, Punto p, String archivo_textura, Material material, Color c, double rotacion, double traslacion, Estrella estrella, HashMap<String,Satelite> satelites) {
+        super(nombre, radio, distancia, p, archivo_textura, material, c, rotacion, traslacion );
+        this.estrella = estrella;
+        this.satelites = satelites;
+    }
+    public Planeta(String nombre, double radio, double distancia, Punto p, Estrella estrella, HashMap<String,Satelite> satelites){
+        super(nombre, radio, distancia, p);
+        this.estrella = estrella;
+        this.satelites = satelites;
+    }
+    public Planeta(String nombre, double radio, double distancia, Punto p){
+        super(nombre, radio, distancia, p);
+        this.estrella = null;
+        this.satelites = new HashMap<String,Satelite>();
     }
     
+    public void addSatelite(Satelite s){
+        satelites.put(s.getNombre(),s);
+    }
+    
+    public boolean hasSatelites(){
+        return satelites.size() > 0;
+    }
+    
+    public int getNumSatelites(){
+        return satelites.size();
+    }
+    
+    public HashMap<String, Satelite> getSatelites(){
+        return satelites;
+    }
+    
+    public ArrayList<Satelite> getSatelitesArray(){
+        ArrayList<Satelite> sats = new ArrayList<Satelite>();
+        
+        for (Satelite value : satelites.values()) {
+            sats.add(value);
+        }
+        
+        return sats;
+    }
+    
+    public Estrella getEstrella(){
+        return estrella;
+    }
+    
+    public void setEstrella(Estrella estrella){
+        this.estrella = estrella;
+    }
    
     
 }
