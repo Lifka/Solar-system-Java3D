6dbd0481a39f99dc8ea5f5dc12605163593fbe5f
jcgallardo <jcgallardomorales@gmail.com>
2016-04-06 11:43:45 +0200

    Implementación Universo

--- SistemaSolar/src/sistemaSolar/Universo.java
+++ SistemaSolar/src/sistemaSolar/Universo.java
@@ -6,6 +6,49 @@
  */
 package sistemaSolar;
 
+import com.sun.j3d.utils.image.TextureLoader;
+import java.util.ArrayList;
+import javax.media.j3d.Texture;
+
 public class Universo {
+    private Nave nave;
+    private ArrayList<Astro> astros;
+    private Texture textura;
     
+    public Universo (String archivo_text){
+        this.textura = new TextureLoader(archivo_text, null).getTexture();
+        this.nave = null;
+        this.astros = null;
+    }
+    
+    public int getNumComponentes(){
+        int total = astros.size();
+        if (nave != null)
+            total++;
+        return total;
+    }
+
+    public Nave getNave() {
+        return nave;
+    }
+
+    public void setNave(Nave nave) {
+        this.nave = nave;
+    }
+
+    public ArrayList<Astro> getAstros() {
+        return astros;
+    }
+
+    public void setAstros(ArrayList<Astro> astros) {
+        this.astros = astros;
+    }
+    
+    public void addAstro(Astro ast){
+        astros.add(ast);
+    }
+    
+    public int numAstros(){
+        return astros.size();
+    }
 }
