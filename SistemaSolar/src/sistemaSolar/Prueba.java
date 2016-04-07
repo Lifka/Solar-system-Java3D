641f3dfa9f6ac7d7ffae3ee1a3a8d43d85831060
Lifka <javierizquierdovera@gmail.com>
2016-04-07 12:31:03 +0200

    Universe

--- SistemaSolar/src/sistemaSolar/Prueba.java
+++ SistemaSolar/src/sistemaSolar/Prueba.java
@@ -6,13 +6,19 @@
  */
 package sistemaSolar;
 
+import com.sun.j3d.utils.geometry.ColorCube;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 import com.sun.j3d.utils.geometry.Sphere;
 import javax.media.j3d.Appearance;
+import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
+import javax.media.j3d.DirectionalLight;
 import javax.media.j3d.PolygonAttributes;
 import javax.media.j3d.TransformGroup;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3f;
 
 public class Prueba {
 
@@ -25,9 +31,73 @@ public class Prueba {
         Visualization visualizationWindows = new Visualization(canvas);
         
         Universo universe = new Universo(new String(), canvas);
-        //universe.getViewingPlatform().setNominalViewingTransform();
         
-        Appearance apariencia = new Appearance();
+        SimpleUniverse simpleUniverse = universe.createUniverso();
+        
+        
+        
+        // TODO
+        BranchGroup raiz = new BranchGroup();
+        
+         Sphere sphere = new Sphere(0.5f);
+        raiz.addChild(sphere);
+     
+        
+        Color3f color = new Color3f(4.0f, -7.0f, -12.0f);
+        Vector3f direction = new Vector3f(5, -6, -10);
+        DirectionalLight light = new DirectionalLight(color, direction);
+        BoundingSphere bounds = new BoundingSphere(new Point3d(0,0,0), 100.0);
+        light.setInfluencingBounds(bounds);
+        raiz.addChild(light);
+        
+        simpleUniverse.getViewingPlatform().setNominalViewingTransform();
+       
+        simpleUniverse.addBranchGraph(raiz);
+        
+        
+ 
+
+        
+       // Branch de planetas
+       /* BranchGroup planetas = new BranchGroup();
+        
+        // Enganchar los planetas al universo
+        simpleUniverse.addBranchGraph(planetas);
+        
+        /*************/
+        /* PLANETAS */
+        /***********/
+        
+        /* Mercurio */
+        
+        // Mercurio es transformable
+      /*  TransformGroup conjuntoMercurio = new TransformGroup ();
+        conjuntoMercurio.setCapability(TransformGroup .ALLOW_TRANSFORM_WRITE);
+        
+        // Enganchar conjuntoMercurio del branch de planetas
+        planetas.addChild(conjuntoMercurio);
+        //conjuntoMercurio.setTransform(Transformacion);
+        
+        /* Venus */
+        
+        // Venus es transformable
+     /*   TransformGroup conjuntoVenus = new TransformGroup ();
+        conjuntoMercurio.setCapability(TransformGroup .ALLOW_TRANSFORM_WRITE);
+        
+        // Enganchar conjuntoVenus del branch de planetas
+        planetas.addChild(conjuntoVenus);
+        //conjuntoVenus.setTransform(Transformacion);
+        
+        /* Tierra */
+        
+        // Tierra es transformable
+   /*     TransformGroup conjuntoTierra = new TransformGroup ();
+        conjuntoMercurio.setCapability(TransformGroup .ALLOW_TRANSFORM_WRITE);
+        
+        // Enganchar conjuntoTierra del branch de planetas
+        planetas.addChild(conjuntoTierra);
+        //conjuntoTierra.setTransform(Transformacion);
+        */
         
       
       
--- SistemaSolar/src/sistemaSolar/Universo.java
+++ SistemaSolar/src/sistemaSolar/Universo.java
@@ -6,16 +6,25 @@
  */
 package sistemaSolar;
 
+import com.sun.j3d.utils.behaviors.vp.OrbitBehavior;
+import com.sun.j3d.utils.geometry.ColorCube;
 import com.sun.j3d.utils.image.TextureLoader;
+import com.sun.j3d.utils.universe.SimpleUniverse;
+import com.sun.j3d.utils.universe.Viewer;
 import com.sun.j3d.utils.universe.ViewingPlatform;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.HashMap;
+import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
 import javax.media.j3d.Material;
 import javax.media.j3d.Texture;
+import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+import javax.media.j3d.View;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
 
 public class Universo {
     private Nave nave;
@@ -25,7 +34,7 @@ public class Universo {
     
 
     public Universo (String archivo_text,Canvas3D aCanvas){
-        this.textura = new TextureLoader(archivo_text, null).getTexture();
+//        this.textura = new TextureLoader(archivo_text, null).getTexture();
         this.nave = null;
         this.astros = new HashMap<String,Astro>();
         this.aCanvas = aCanvas;
@@ -87,52 +96,45 @@ public class Universo {
     
     
     
-    public void createUniverso(){
+    public SimpleUniverse createUniverso(){
 
-        ViewingPlatform viewnzgPlatform = new ViewingPlatform ( ) ;
+        
+        /***********    VIEWINGPLATFORM   **********/
+        
+        // Creamos lo que hace que se vean las cosas, manualmente, para poder asignarlo
+        // y personalizarlo
+        ViewingPlatform viewingPlatform = new ViewingPlatform ();
+        // Radio de activación
+        viewingPlatform.getViewPlatform().setActivationRadius(100f);
+        
+        // Transformación de vista -> dónde se está , a dónde se mira , Vup
+        TransformGroup viewTransformGroup = viewingPlatform.getViewPlatformTransform();
+        Transform3D viewTransform3D = new Transform3D ( );
+        viewTransform3D.lookAt (new Point3d (20 ,20 ,20 ),
+            new Point3d (0, 0, 0), new Vector3d (0 ,1 ,0));
+        
+        viewTransform3D.invert();
+        viewTransformGroup .setTransform(viewTransform3D);
+        
+        // Mover la cámara con el ratón
+        OrbitBehavior orbit = new OrbitBehavior (aCanvas, OrbitBehavior .REVERSE_ALL);
+        BoundingSphere bound = new BoundingSphere (new Point3d (0.0f, 0.0f , 0.0f) , 100.0f);
+        orbit.setSchedulingBounds(bound);
+        
+        orbit.setZoomFactor(2.0f);
+        
+        viewingPlatform. setViewPlatformBehavior(orbit);
+    
+        
+        /**************   VIEWER     **************/
+        
+        Viewer viewer = new Viewer(aCanvas);
+        View view = viewer.getView();
+        view.setFieldOfView(Math.toRadians(45));
+        view.setBackClipDistance(50.0);
+        
+        return new SimpleUniverse(viewingPlatform, viewer);
 
-      
-
-       // Branch de planetas
-        BranchGroup planetas = new BranchGroup();
-        
-        // Enganchar los planetas al universo
-        addBranchGraph(planetas);
-        
-        /*************/
-        /* PLANETAS */
-        /***********/
-        
-        /* Mercurio */
-        
-        // Mercurio es transformable
-        TransformGroup conjuntoMercurio = new TransformGroup ();
-        conjuntoMercurio.setCapability(TransformGroup .ALLOW_TRANSFORM_WRITE);
-        
-        // Enganchar conjuntoMercurio del branch de planetas
-        planetas.addChild(conjuntoMercurio);
-        //conjuntoMercurio.setTransform(Transformacion);
-        
-        /* Venus */
-        
-        // Venus es transformable
-        TransformGroup conjuntoVenus = new TransformGroup ();
-        conjuntoMercurio.setCapability(TransformGroup .ALLOW_TRANSFORM_WRITE);
-        
-        // Enganchar conjuntoVenus del branch de planetas
-        planetas.addChild(conjuntoVenus);
-        //conjuntoVenus.setTransform(Transformacion);
-        
-        /* Tierra */
-        
-        // Tierra es transformable
-        TransformGroup conjuntoTierra = new TransformGroup ();
-        conjuntoMercurio.setCapability(TransformGroup .ALLOW_TRANSFORM_WRITE);
-        
-        // Enganchar conjuntoTierra del branch de planetas
-        planetas.addChild(conjuntoTierra);
-        //conjuntoTierra.setTransform(Transformacion);
-        
     }
 
 }
